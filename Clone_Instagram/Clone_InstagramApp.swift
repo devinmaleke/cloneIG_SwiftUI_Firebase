2c8ff86b3fdb157724d9c1419de8dc0c65908884
Devin <devin.maleke@gmail.com>
2023-08-20 15:49:49 +0700

    Initial Commit

---
+++ Clone_Instagram/Clone_InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  Clone_InstagramApp.swift
+//  Clone_Instagram
+//
+//  Created by Devin Maleke on 20/08/23.
+//
+
+import SwiftUI
+
+@main
+struct Clone_InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
